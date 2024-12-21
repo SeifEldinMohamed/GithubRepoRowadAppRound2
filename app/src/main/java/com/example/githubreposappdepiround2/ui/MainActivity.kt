275035f21fd1a513419c640f4f0783eea4ad310f
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-12-21 20:20:01 +0200

    - add dependencies and themes for our app

---
+++ app/src/main/java/com/example/githubreposappdepiround2/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.githubreposappdepiround2.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.githubreposappdepiround2.navigation.AppNavHost
+import com.example.githubreposappdepiround2.ui.theme.GithubRepoAppDEPIRound2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GithubRepoAppDEPIRound2Theme {
+                AppNavHost()
+            }
+        }
+    }
+}
